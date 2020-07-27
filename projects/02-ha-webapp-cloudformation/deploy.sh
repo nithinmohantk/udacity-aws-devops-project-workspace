18fc673224061d65ab963c9842c6769d9570fd30
Nithin Mohan T K <nithin@thingx.cloud>
2020-07-27 10:02:01 +0100

    updated the udagram.zip

--- projects/02-ha-webapp-cloudformation/deploy.sh
+++ projects/02-ha-webapp-cloudformation/deploy.sh
@@ -56,20 +56,4 @@ while [ "$1" != "" ]; do
     shift
 done
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -f | --file )           shift
-                                filename=$1
-                                ;;
-        -i | --interactive )    interactive=1
-                                ;;
-        -h | --help )           usage
-                                exit
-                                ;;
-        * )                     usage
-                                exit 1
-    esac
-    shift
-done
-
 # ./deploy.sh -c "udagram-dev" "cf-ha-web-infra.yml" "cf-ha-web-infra.parameters.json"
