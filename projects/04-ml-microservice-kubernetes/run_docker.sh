edaa59af6696a0b6fe9ff9c528c90addec6770bf
Nithin Mohan T K <nithinmohantk@thingx-lab-ubuntu.localdomain>
2020-07-31 01:41:45 -0700

    updated Dockerfile, docker specs and kube

--- projects/04-ml-microservice-kubernetes/run_docker.sh
+++ projects/04-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
+docker image build -t udacity-boston-housing:latest .
 
-# Step 2: 
 # List docker images
+docker image ls
 
-# Step 3: 
 # Run flask app
+docker run -d -p 8000:80 udacity-boston-housing:latest
+
+# List docker process
+docker ps -a
--- projects/04-ml-microservice-kubernetes/upload_docker.sh
+++ projects/04-ml-microservice-kubernetes/upload_docker.sh
@@ -3,13 +3,17 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
+USERNAME=$1
+
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=${USERNAME}/udacity-boston-housing
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u "${USERNAME}"
+docker tag boston-housing $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath:latest
