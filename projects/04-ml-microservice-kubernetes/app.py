8aaf5c853c161f8df5e9de9c52210594f074f83a
Nithin Mohan T K <nithin@thingx.cloud>
2020-08-01 16:02:46 +0100

    successfully made the docker image

--- projects/04-ml-microservice-kubernetes/app.py
+++ projects/04-ml-microservice-kubernetes/app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = f"<h3>Boston Housing Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -62,11 +62,10 @@ def predict():
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
     LOG.info(f"Prediction Value: \n{prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=8000, debug=True) # specify port=80
