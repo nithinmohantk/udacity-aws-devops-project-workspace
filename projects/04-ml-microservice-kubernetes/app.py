d3aa2bd1a46420da62bfcacf5d5e69cc895cdea9
Nithin Mohan T K <nithin@thingx.cloud>
2020-08-02 14:57:46 +0100

    added some ignore rules

--- projects/04-ml-microservice-kubernetes/app.py
+++ projects/04-ml-microservice-kubernetes/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
-
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
@@ -12,8 +11,7 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-    
-    LOG.info('Scaling Payload: \n %s',payload)
+    LOG.info(f'Scaling Payload: \n %s', payload)
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
@@ -26,7 +24,6 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
     """Performs an sklearn prediction
-        
         input looks like:
         {
         "CHAS":{
@@ -47,22 +44,19 @@ def predict():
         "LSTAT":{
         "0":4.98
         }
-        
         result looks like:
         { "prediction": [ <val> ] }
-        
         """
-    
     # Logging the input payload
     json_payload = request.json
-    LOG.info('JSON payload: \n %s', json_payload)
+    LOG.info(f'JSON payload: \n %s', json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info('Inference payload DataFrame: \n %s', inference_payload)
+    LOG.info(f'Inference payload DataFrame: \n %s', inference_payload)
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    LOG.info('Prediction Value: \n %s', prediction)
+    LOG.info(f'Prediction Value: \n %s', prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
